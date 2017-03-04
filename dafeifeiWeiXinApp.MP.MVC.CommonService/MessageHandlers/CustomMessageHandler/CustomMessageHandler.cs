4931370599cdaff363404882fadaa18434b2ed8d
dafeifei0218 <ytr19860218@163.com>
2017-03-04 22:47:45 +0800

    注释代码

--- dafeifeiWeiXinApp.MP.MVC.CommonService/MessageHandlers/CustomMessageHandler/CustomMessageHandler.cs
+++ dafeifeiWeiXinApp.MP.MVC.CommonService/MessageHandlers/CustomMessageHandler/CustomMessageHandler.cs
@@ -21,16 +21,16 @@ namespace DFF.WeiXinApp.MP.MVC.CommonService.CustomMessageHandler
 
         public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
         {
-            var responseMessage = base.CreateResponseMessage<ResponseMessageText>();
+            var responseMessage = base.CreateResponseMessage<ResponseMessageText>(); //ResponseMessageText也可以是News等其他类型
             responseMessage.Content = "这条消息来自DefaultResponseMessage。";
             return responseMessage;
         }
 
         public override IResponseMessageBase OnTextRequest(RequestMessageText requestMessage)
         {
-            var responseMessage = base.CreateResponseMessage<ResponseMessageText>();
-            responseMessage.Content = "您的OpenID是：" + requestMessage.FromUserName
-                + "。\r\n您发送了文字信息：" + requestMessage.Content;
+            var responseMessage = base.CreateResponseMessage<ResponseMessageText>(); //ResponseMessageText也可以是News等其他类型
+            responseMessage.Content = "您的OpenID是：" + requestMessage.FromUserName  //这里的requestMessage.FromUserName也可以直接携程base.WeixinOpenId
+                + "。\r\n您发送了文字信息：" + requestMessage.Content;                //\r\n用于执行，requestMessage.Content即用户发过来的文字内容
             return responseMessage;
         }
     }
--- dafeifeiWeiXinApp.MP.MVC/Controllers/WeixinController.cs
+++ dafeifeiWeiXinApp.MP.MVC/Controllers/WeixinController.cs
@@ -17,8 +17,67 @@ namespace DFF.WeiXinApp.MP.MVC.Controllers
 {
     public class WeixinController : Controller
     {
+        #region 验证登录
+
+        //public static readonly string Token = "YourToken";//与微信公众账号后台的Token设置保持一致，区分大小写。
+        //public static readonly string EncodingAESKey = "YourKey";//与微信公众账号后台的EncodingAESKey设置保持一致，区分大小写。
+        //public static readonly string AppId = "YourAppId";//与微信公众账号后台的AppId设置保持一致，区分大小写。
+
+        /// <summary>
+        /// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://weixin.senparc.com/weixin
+        /// </summary>
+        /// <param name="postModel">微信公众服务器加密参数集合</param>
+        /// <param name="echostr"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [ActionName("Index")]
+        //public ActionResult Index(string signature, string timestamp, string nonce, string echostr)
+        public ActionResult Index(PostModel postModel, string echostr)
+        {
+            string token = ConfigurationManager.AppSettings["Token"].ToString();
+
+            if (CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, token))
+            {
+                return Content(echostr);
+            }
+            else
+            {
+                return Content("failed:" + postModel.Signature + "," + Senparc.Weixin.MP.CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, token) + "。" +
+                    "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="postModel"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ActionName("Index")]
+        public ActionResult Post(PostModel postModel)
+        {
+            string token = ConfigurationManager.AppSettings["Token"].ToString();
+            string encodingAEKey = ConfigurationManager.AppSettings["EncodingAESKey"].ToString();
+            string appId = ConfigurationManager.AppSettings["AppId"].ToString();
+            if (!CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, token))
+            {
+                return Content("参数错误！");
+            }
+
+            postModel.Token = token;
+            postModel.EncodingAESKey = encodingAEKey;
+            postModel.AppId = appId;
+
+            var messageHandler = new CustomMessageHandler(Request.InputStream, postModel);
+            messageHandler.Execute();
+
+            return new FixWeixinBugWeixinResult(messageHandler);
+        }
+
+        #endregion
+
         #region 创建菜单
-        
+
         /// <summary>
         /// 创建菜单
         /// </summary>
@@ -85,64 +144,6 @@ namespace DFF.WeiXinApp.MP.MVC.Controllers
             ViewData["Menu"] = result02.menu;
 
             return View();
-        } 
-
-        #endregion
-
-        #region 验证登录
-
-        //public static readonly string Token = "YourToken";//与微信公众账号后台的Token设置保持一致，区分大小写。
-        //public static readonly string EncodingAESKey = "YourKey";//与微信公众账号后台的EncodingAESKey设置保持一致，区分大小写。
-        //public static readonly string AppId = "YourAppId";//与微信公众账号后台的AppId设置保持一致，区分大小写。
-
-        /// <summary>
-        /// 微信后台验证地址（使用Get），微信后台的“接口配置信息”的Url填写如：http://weixin.senparc.com/weixin
-        /// </summary>
-        /// <param name="postModel"></param>
-        /// <param name="echostr"></param>
-        /// <returns></returns>
-        [HttpGet]
-        [ActionName("Index")]
-        //public ActionResult Index(string signature, string timestamp, string nonce, string echostr)
-        public ActionResult Index(PostModel postModel, string echostr)
-        {
-            string token = ConfigurationManager.AppSettings["Token"].ToString();
-
-            if (CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, token))
-            {
-                return Content(echostr);
-            }
-            else
-            {
-                return Content("failed:" + postModel.Signature + "," + Senparc.Weixin.MP.CheckSignature.GetSignature(postModel.Timestamp, postModel.Nonce, token) + "。" +
-                    "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        [HttpPost]
-        [ActionName("Index")]
-        public ActionResult Post(PostModel postModel)
-        {
-            string token = ConfigurationManager.AppSettings["Token"].ToString();
-            string encodingAEKey = ConfigurationManager.AppSettings["EncodingAESKey"].ToString();
-            string appId = ConfigurationManager.AppSettings["AppId"].ToString();
-            if (!CheckSignature.Check(postModel.Signature, postModel.Timestamp, postModel.Nonce, token))
-            {
-                return Content("参数错误！");
-            }
-
-            postModel.Token = token;
-            postModel.EncodingAESKey = encodingAEKey;
-            postModel.AppId = appId;
-
-            var messageHandler = new CustomMessageHandler(Request.InputStream, postModel);
-            messageHandler.Execute();
-
-            return new FixWeixinBugWeixinResult(messageHandler);
         }
 
         #endregion
@@ -162,7 +163,7 @@ namespace DFF.WeiXinApp.MP.MVC.Controllers
         //        //
         //        if (CheckSignature(signature, timestamp, nonce, token))
         //        {
-        //            WriteContent();//
+        //            WriteContent();
         //        }
         //        else
         //        {
@@ -197,10 +198,18 @@ namespace DFF.WeiXinApp.MP.MVC.Controllers
 
         #endregion
 
+        #region 二维码
+
+        /// <summary>
+        /// 二维码
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ErWeiCode()
         {
 
             return View();
-        }
+        } 
+
+        #endregion
     }
 }
